18b7fe28d807b9f20c5a700ce3c55f357a353827
chipmunk-dev <devquokkajeong@gmail.com>
2023-01-26 21:03:05 +0900

    Learn adapter pattern

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
+import adapter.*;
 import singleton.AClazz;
 import singleton.BClazz;
 import singleton.SocketClient;
 
 public class Main {
     public static void main(String[] args) {
+        /*
         AClazz aClazz = new AClazz();
         BClazz bClazz = new BClazz();
 
@@ -11,5 +13,24 @@ public class Main {
         SocketClient bSocketClient = bClazz.getSocketClient();
 
         System.out.println("동일한 클라이언트인가 = " + aSocketClient.equals(bSocketClient));
+        */
+
+        HairDryer hairDryer = new HairDryer();
+        connect(hairDryer);
+
+        Cleaner cleaner = new Cleaner();
+        //connect(cleaner);
+        Electronic110V adapter = new SocketAdapter(cleaner);
+        connect(adapter);
+
+        AirConditioner airConditioner = new AirConditioner();
+        Electronic110V airAdapter = new SocketAdapter(airConditioner);
+        connect(airAdapter);
+
+    }
+
+    // 콘센트
+    public static void connect(Electronic110V electronic110V) {
+        electronic110V.powerOn();
     }
 }
