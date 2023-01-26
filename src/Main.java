015ecc7497c751c58ed0377bc38a0c5104fe853d
chipmunk-dev <devquokkajeong@gmail.com>
2023-01-26 21:34:51 +0900

    Learn Proxy Pattern

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 import adapter.*;
+import aop.AopBrowser;
+import proxy.Browser;
+import proxy.BrowserProxy;
+import proxy.IBrowser;
 import singleton.AClazz;
 import singleton.BClazz;
 import singleton.SocketClient;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,7 +20,7 @@ public class Main {
 
         System.out.println("동일한 클라이언트인가 = " + aSocketClient.equals(bSocketClient));
         */
-
+        /*
         HairDryer hairDryer = new HairDryer();
         connect(hairDryer);
 
@@ -26,6 +32,37 @@ public class Main {
         AirConditioner airConditioner = new AirConditioner();
         Electronic110V airAdapter = new SocketAdapter(airConditioner);
         connect(airAdapter);
+        */
+
+        /*
+        // IBrowser browser = new Browser("www.naver.com");
+        IBrowser browser = new BrowserProxy("www.naver.com");
+        browser.show();
+        browser.show();
+        browser.show();
+        browser.show();
+        */
+
+        AtomicLong start = new AtomicLong();
+        AtomicLong end = new AtomicLong();
+
+        IBrowser aopBrowser = new AopBrowser("www.naver.com",
+                () -> {
+                    System.out.println("before");
+                    start.set(System.currentTimeMillis());
+                },
+                () -> {
+                    long now = System.currentTimeMillis();
+                    end.set(now - start.get());
+                }
+        );
+
+        aopBrowser.show();
+        System.out.println(end.get());
+
+        aopBrowser.show();
+        System.out.println(end.get());
+
 
     }
 
